471c15b6c117826cc8481bf8308d077f693a559b
Carlos Ovalle <carlos.ovalle21@utexas.edu>
2021-08-23 15:25:53 -0500

    shopping cart done

---
+++ Phase 2/shopping-cart/main.js
@@ -0,0 +1,39 @@
+let stdNames = ["Raj", "Seeta", "Reeta", "Meeta", "Veeta"];
+
+var data = new Array();
+
+function updateArraySize(){
+	document.getElementById("cartSize").innerHTML="New Cart Size: " + data.length;
+}
+
+function getData(){
+    var str = localStorage.getItem("localData");
+    if (str!= null)
+        data = JSON.parse(str);
+}
+
+function addToCart(product_name, price){
+	getData();
+	data.push({name:product_name,costs:price});
+	localStorage.setItem("localData",JSON.stringify(data));
+	updateArraySize();
+}
+
+
+function displayData() {
+	getData();
+	
+	let table = `<table border=1>
+                            	<tr>
+                                	<th>Name</th>
+                                	<th>Cost</th>
+                            	</tr>
+                            	`;
+    var total = 0;                        	
+	for(var i = 0; i < data.length; i++){
+		table +="<tr><td>" + data[i].name +"</td><td>"+ data[i].costs+ "</td></tr>";
+		total += parseInt(data[i].costs);
+	}
+    table +="</table>";
+    document.getElementById("checkout").innerHTML=table + "<br>TOTAL COST:" + total;
+}
